b3f31dc0f51310e599383f108df516140072294f
Khulan Batbayar <khulan.msc.2012@gmail.com>
2018-03-12 12:32:22 +0100

    Added log file output

--- main.py
+++ main.py
@@ -78,17 +78,18 @@ class Client():
         command = Popen(shlex.split(cmd),stdout=PIPE, stderr=PIPE)
         stdout, stderr = command.communicate()
         lat, status = stdout.decode("utf-8").split(',')
-        if(int(status) != 200):
-            print('removing ',':', gateway.address)
+        if(int(status) != 200):            
+            with open('log_output','a') as f:                
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+', removing:'+gateway.address+'\n')
             self.removeGateway(gateway)
         else:
             gateway.ts = datetime.datetime.now()
             gateway.latency = float(lat)      
             self.setCategory(gateway)
-            print(gateway.address,':',gateway.ts,':',float(lat), ':', int(status))
+            #print(gateway.address,':',gateway.ts,':',float(lat), ':', int(status))
     
     def pingTest(self, address):
-        response = subprocess.call(['ping', '-q', '-c', '1', '-w','5',address], stdout=PIPE)
+        response = subprocess.call(['ping', '-q', '-c', '1', '-w','5',address], stdout=subprocess.DEVNULL)
         if response == 0:
             return True
         else:
@@ -98,8 +99,9 @@ class Client():
         threading.Timer(120.0, self.updateGoodGateways).start()
         goodGateways = [gw for gw in self.gateways if gw.status is True]
         print(len(goodGateways))
-        for randomGw in self.select2Random(goodGateways):
-            print('pinging good:',randomGw.address)
+        for randomGw in self.select2Random(goodGateways):            
+            with open('log_output','a') as f:                
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+', pinging good: '+randomGw.address+'\n')
             self.pingGateway(randomGw)
 
     def updateBadGateways(self):
@@ -107,7 +109,8 @@ class Client():
         badGateways = [gw for gw in self.gateways if gw.status is False]
         print(len(badGateways))
         for randomGw in self.select2Random(badGateways):
-            print('pinging bad:',randomGw.address)
+            with open('log_output','a') as f:                
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+', pinging bad: '+randomGw.address+'\n' )
             self.pingGateway(randomGw)
     
     def setCategory(self, gw):        
@@ -138,43 +141,47 @@ class Client():
         return selected
     
     def selectBest(self):
-        selected = None
+        selected = self.defaultGateway
         for gw in self.gateways:
-            if selected is None:
-                selected = gw
-                continue
             if gw.latency < selected.latency:
                 selected = gw                
         self.defaultGateway = selected
     
     def selectNext(self):
         if(self.gateways[0].address == self.defaultGateway.address):
-            self.defaultGateway = self.gateways[0]
-            self.gateways.remove(self.gateways[0])
-            self.append(self.defaultGateway)        
-        self.defaultGateway = self.gateways[1]
+            self.gateways.remove(self.defaultGateway)
+            self.gateways.append(self.defaultGateway)
+        self.defaultGateway = self.gateways[0]
         
-    
+    #def connectBest(self):
+        
     def connect(self):
+        threading.Timer(60.0, self.connect).start()
         #Checking if gw is responsive, connectable
         if self.pingTest(self.defaultGateway.address) is False:
-            print('Changing the unresponsive gw:', self.defaultGateway.address)
-            self.removeGateway(self.defaultGateway)
+            with open('log', 'a') as f:
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+':  Changing the unresponsive gw::'+self.defaultGateway.address)
+                f.close()
             self.selectNext()
-        print('Connecting to:', self.defaultGateway.address)
+            return             
         
-        threading.Timer(60.0, self.connect).start()
         cmd='''curl -x '''+self.defaultGateway.address+''':3128 -U david.pinilla:"|Jn 5DJ\\7inbNniK|m@^ja&>C" -m 180 -w %{time_starttransfer},%{time_total},%{http_code} http://ovh.net/files/1Mb.dat -o /dev/null -s'''
         command = Popen(shlex.split(cmd),stdout=PIPE, stderr=PIPE)
         stdout, stderr = command.communicate()
         ttfb, lat, status = stdout.decode("utf-8").split(',')
         if(int(status) == 0):
-            print('removing ',':', self.defaultGateway.address)
-            self.removeGateway(self.defaultGateway)
+            with open('log', 'a') as f:
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': Return status 0:'+self.defaultGateway.address)
+                f.close()
+            #self.removeGateway(self.defaultGateway)
             self.selectNext()
+            return
         elif(int(status)!= 200):
-            print('changing GW ERROR ',':', self.defaultGateway.address)
+            with open('log', 'a') as f:
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': changing GW ERROR'+self.defaultGateway.address)
+                f.close()
             self.selectNext()
+            return
         else:
             self.defaultGateway.ts = datetime.datetime.now()
             self.defaultGateway.latency = float(lat)      
@@ -182,8 +189,7 @@ class Client():
         with open('download_manual','a') as f:
             f.write("{0},{1},{2},{3}\n".format(self.defaultGateway.ts,
                                                    self.defaultGateway.address,float(lat),int(status)))
-        
-                
+                      
 node1 = Gateway('10.139.40.85', 0.15, '2018 Feb 15 18:59:15', False)
 node2 = Gateway('10.139.40.122', 0.20, '2018 Feb 15 18:20:15',False)
 node3 = Gateway('10.138.57.2', 0.02, '2018 Feb 15 18:45:07', False)
@@ -202,6 +208,8 @@ for gw in client4.gateways:
         if gw.address == client4.defaultGateway.address:
             client4.selectNext()
         client4.removeGateway(gw)
+        
+with open('log', 'a') as f:
+    f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': Connecting with '+str(len(client4.gateways))+' gateways')
+    f.close()
 client4.connect()
-#client4.updateGoodGateways()
-#client4.updateBadGateways()
