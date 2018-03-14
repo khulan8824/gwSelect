3de44ee2e1152d678ba5423729eca283d40b319e
Khulan Batbayar <khulan.msc.2012@gmail.com>
2018-03-14 14:51:36 +0100

    Random selection of 2

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ class Gateway():
         self.ts = ts
         self.status = status
         
+#repr, cmp and getKey are the helper functions for sorting Gateway class objects
+    def __repr__(self):
+        return '{}, {},{}, {}'.format(self.__class__.__name__, self.ts, self.address, self.latency)
+    
+    def __cmp__(self, other):
+        if hasattr(other, 'latency'):
+            return self.latency.__cmp__(other.latency)
+    
+    def getKey(self):
+        return self.latency
+        
     def setStatus(self, status):
         self.status=status
     
@@ -38,6 +49,7 @@ class Client():
     address = ""
     neighbours = []
     gateways = []
+    rank = []
     defaultGateway = None
     round = 0
     
@@ -46,6 +58,7 @@ class Client():
         self.neighbours = neighbours
         self.gateways = gateways
         self.defaultGateway = defaultGateway
+        
     
     def getDefaultGateway(self):
         return self.defaultGateway
@@ -69,15 +82,15 @@ class Client():
     def updateGateway(self, gateway):
         gw = next(x for x in self.gateways if x.address == gateway.address)
     
-    def printGatewayInformation(self):
-        
+    def printGatewayInformation(self):        
         with open('log', 'a') as f:
             f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+', Total number:'+str(len(self.gateways))+'\n')
             for gw in self.gateways:
                 f.write(gw.address+','+str(gw.latency)+':'+ gw.ts.strftime("%Y-%m-%d %H:%M:%S")+':'+ str(gw.status)+'\n')
             f.close()
-    
-    def pingGateway(self, gateway):        
+            
+    def pingGateway(self, gateway):
+        status = True
         cmd='''curl -x '''+gateway.address+''':3128 -U david.pinilla:"|Jn 5DJ\\7inbNniK|m@^ja&>C" -m 180 -w %{time_total},%{http_code} http://ovh.net/files/1Mb.dat -o /dev/null -s'''
         command = Popen(shlex.split(cmd),stdout=PIPE, stderr=PIPE)
         stdout, stderr = command.communicate()
@@ -86,70 +99,82 @@ class Client():
             with open('log','a') as f:                
                 f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+', removing:'+gateway.address+'\n')
             self.removeGateway(gateway)
+            status = False
         elif(int(status)!=200):
             gateway.ts = datetime.datetime.now()
             gateway.latency = 200
-            self.setCategory(gateway)
+            status = False
         else:
             gateway.ts = datetime.datetime.now()
             gateway.latency = float(lat)      
-            self.setCategory(gateway)
+        return status
     
     def pingTest(self, address):
-        response = subprocess.call(['ping', '-q', '-c', '1', '-w','5',address], stdout=subprocess.PIPE)
+        response = subprocess.call(['ping', '-q', '-c', '1', '-w','5',address], stdout=PIPE)
         if response == 0:
             return True
         else:
             return False
-    
-    
-    def setCategory(self, gw):        
-        if gw.latency < 3:
-            gw.status= True
+      
+        
+    def selectBest(self, gws):
+        if len(gws) == 1:
+            self.defaultGateway = gws[0]
+            return True
+        
+        for gw in gws:
+            if self.pingGateway(gw) is False:
+                return False
+        if gws[0].latency > gws[1].latency:
+            self.defaultGateway = gws[1]
         else:
-            gw.status = False
-        
-        
-    def selectBest(self):
-        selected = self.defaultGateway
-        for gw in self.gateways:
-            if gw.latency < selected.latency:
-                selected = gw                
-        self.defaultGateway = selected
+            self.defaultGateway = gws[0]
+        return True
     
-    def senseGateways(self):        
+    #GATEWAY SELECTION LOGIC
+    def select2Random(self):
+        #return random.sample(set([gw for gw in gateways if gw.status is True]), 2)
+        if len(self.gateways)>2:
+            return random.sample(set(self.gateways), 2)
+        else:
+            return self.gateways
+
+    def senseGateways(self):
         threading.Timer(300.0, self.senseGateways).start()
-        for gw in self.gateways:
-            self.pingGateway(gw)
-        self.selectBest()
+        status = False
+        while(status is not True):
+            status =self.selectBest(self.select2Random())
         self.printGatewayInformation()
-        
+
     def connect(self):
-        threading.Timer(60.0, self.connect).start()
+        threading.Timer(60.0, self.connect).start()        
+        
         cmd='''curl -x '''+self.defaultGateway.address+''':3128 -U david.pinilla:"|Jn 5DJ\\7inbNniK|m@^ja&>C" -m 180 -w %{time_starttransfer},%{time_total},%{http_code} http://ovh.net/files/1Mb.dat -o /dev/null -s'''
         command = Popen(shlex.split(cmd),stdout=PIPE, stderr=PIPE)
         stdout, stderr = command.communicate()
         ttfb, lat, status = stdout.decode("utf-8").split(',')
-        with open('download_best','a') as f:
+        with open('download_random2','a') as f:
             f.write("{0},{1},{2},{3},{4}\n".format(datetime.datetime.now(),self.round,
                                                    self.defaultGateway.address,float(lat),int(status)))
         self.round += 1
                       
-node1 = Gateway('10.139.40.85', 0.15, '2018 Feb 15 18:59:15', False)
-node2 = Gateway('10.139.40.122', 0.20, '2018 Feb 15 18:20:15',False)
-node3 = Gateway('10.138.57.2', 0.02, '2018 Feb 15 18:45:07', False)
-node4 = Gateway('10.228.0.83', 0.5, '2018 Feb 15 18:47:21', False)
-node5 = Gateway('10.138.77.2', 0.78, '2018 Feb 15 18:26:19', False)
-node6 = Gateway('10.138.85.130', 0.35, '2018 Feb 15 19:00:01', True)
-node7 = Gateway('10.139.17.4', 2.1, '2018 Feb 15 18:26:04', False)
-node8 = Gateway('10.139.37.194', 1.5, '2018 Feb 15 18:30:14', True)
-node9 = Gateway('10.138.62.2', 0.04, '2018 Feb 15 19:47:59', False)
-node10 = Gateway('10.138.25.67', 0.09, '2018 Feb 15 17:59:07', False)
+node1 = Gateway('10.139.40.85', 0.15, datetime.datetime.strptime('2018 Feb 15 18:59:15', '%Y %b %d %H:%M:%S'), False)
+node2 = Gateway('10.139.40.122', 0.20, datetime.datetime.strptime('2018 Feb 15 18:20:15', '%Y %b %d %H:%M:%S'),False)
+node3 = Gateway('10.138.57.2', 0.02, datetime.datetime.strptime('2018 Feb 15 18:45:07', '%Y %b %d %H:%M:%S'), False)
+node4 = Gateway('10.228.0.83', 0.5, datetime.datetime.strptime('2018 Feb 15 18:47:21', '%Y %b %d %H:%M:%S'), False)
+node5 = Gateway('10.138.77.2', 0.78, datetime.datetime.strptime('2018 Feb 15 18:26:19', '%Y %b %d %H:%M:%S'), False)
+node6 = Gateway('10.138.85.130', 0.35, datetime.datetime.strptime('2018 Feb 15 19:00:01', '%Y %b %d %H:%M:%S'), True)
+node7 = Gateway('10.139.17.4', 2.1, datetime.datetime.strptime('2018 Feb 15 18:26:04', '%Y %b %d %H:%M:%S'), False)
+node8 = Gateway('10.139.37.194', 1.5, datetime.datetime.strptime('2018 Feb 15 18:30:14', '%Y %b %d %H:%M:%S'), True)
+node9 = Gateway('10.138.62.2', 0.04, datetime.datetime.strptime('2018 Feb 15 19:47:59', '%Y %b %d %H:%M:%S'), False)
+node10 = Gateway('10.138.25.67', 0.09, datetime.datetime.strptime('2018 Feb 15 17:59:07', '%Y %b %d %H:%M:%S'), False)
 
+listGW = [node1,node2,node4, node8, node9, node10, node6,node7, node5, node3]
+client4= Client('10.139.40.77', [], listGW, node7)
 
-client4= Client('10.139.40.77', [], [node1,node2,node4, node8, node9, node10, node6,node7, node5, node3], node7)
 for gw in client4.gateways:
     if client4.pingTest(gw.address) is False:
         client4.removeGateway(gw)
+
 client4.senseGateways()
 client4.connect()
